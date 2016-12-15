0425c4749864d553679bda26f902cc1684e83ba8
Todd Gandee <tgandee@bignerdranch.com>
2016-12-15 10:01:26 -0500

    wrap event in conditional

--- app/components/coffee-order-row.js
+++ app/components/coffee-order-row.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   isComplete: false,
   isCompleteChanged: Ember.observer('isComplete', function() {
-    this.send('removeRow');
+    if(this.get('isComplete')) this.send('removeRow');
   }),
   actions: {
     removeRow() {
