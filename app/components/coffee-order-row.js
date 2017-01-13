e2de3c89b92015259ddd616009beb76eb88b6199
Todd Gandee <tgandee@bignerdranch.com>
2017-01-13 10:28:24 -0500

    add attributes to coffee-lablel-row component

--- app/components/coffee-order-row.js
+++ app/components/coffee-order-row.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  classNames: ["checkbox"],
+  attributeBindings: ["data-coffee-order"],
   isComplete: false,
   isCompleteChanged: Ember.observer('isComplete', function() {
     if(this.get('isComplete')) this.send('removeRow');
